03854a178223ecd44b5424e5d3fe42b897c0b299
Tim Crooker <timothycrooker@gmail.com>
2021-12-08 00:54:57 -0500

    j

--- src/ensureGenerator/index.ts
+++ src/ensureGenerator/index.ts
@@ -1,16 +1,16 @@
 import { ParsedGenerator } from '@/parseGenerator'
 import { colors } from 'swaglog'
-import yt from 'youtill'
+import { pathExists } from 'youtill'
 
 /** Check that the generator exists where it should be and download it if it isnt */
 export const ensureGenerator = async (
 	generator: ParsedGenerator,
 	update?: boolean
 ): Promise<void> => {
-	const exists = await yt.pathExists(generator.path)
+	const exists = await pathExists(generator.path)
 
 	// if the generator already exists and no update is requested, we are done here
-	if ((await yt.pathExists(generator.path)) && !update) return
+	if ((await pathExists(generator.path)) && !update) return
 
 	// if the generator is local but it doesnt exist throw an error
 	if (generator.type === 'local' && !exists) {
